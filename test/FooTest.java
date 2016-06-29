cddf10ed67725ab3e99351d73af5cd5296468b7c
mahadeva <mahadeva@INmahadeva-2.local>
2016-06-29 09:50:26 +0530

    init

---
+++ test/FooTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FooTest {
+    @Test
+    public void should_test_this() {
+        assertEquals(1,2);
+    }
+}
