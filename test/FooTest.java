14a1aaecd5cce7937097a7157465b1f3990ee234
mahadeva <mahadeva@INmahadeva-2.local>
2016-06-30 11:44:03 +0530

    checking in the test

--- test/FooTest.java
+++ test/FooTest.java
@@ -1,10 +1,12 @@
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import static org.junit.Assert.assertEquals;
 
 public class FooTest {
     @Test
     public void should_test_this() {
-        assertEquals(1,2);
+        assertEquals(1, 2);
     }
 }
